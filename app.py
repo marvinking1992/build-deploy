f1995bce1796c1f9eabed539180fb040410ee4ba
marvinking1992 <144110860+marvinking1992@users.noreply.github.com>
2023-09-17 19:14:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
-
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, world!'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+@app.route("/")
+def hello():
+    return "Hello World, with cloud build-deploy!"
+# run the app.
+if __name__ == "__main__":
+    app.run()
